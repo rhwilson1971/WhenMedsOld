d19e431191d24c84c7eb72398d37007554c6e1c8
Reuben Wilson <reuben.wilson@cyn-reub.com>
2015-06-25 15:32:48 -0700

    xcode initial source
    Initial Commit

---
+++ WhenMeds/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhenMeds
+//
+//  Created by Reuben Wilson on 6/25/15.
+//  Copyright (c) 2015 wymsii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
